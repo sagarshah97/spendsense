fbe6a9ba9bd47cbbfdc51d2a89c6820305cde8f5
Sagar Paresh Shah <sg355741@dal.ca>
2023-07-17 21:12:26 -0300

    added root level App.js

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import Router from "./utils/Routes";
+import { BrowserRouter } from "react-router-dom";
+
+function App() {
+  return (
+    <>
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
+    </>
+  );
+}
+
+export default App;
